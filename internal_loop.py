3b036bfd1f4403936afee470eefc4fb17d29dfc9
Micha Gorelick <mynameisfiber@gmail.com>
2015-04-16 13:30:52 -0400

    dealt with relative jumps over bytecode changes

--- internal_loop.py
+++ internal_loop.py
@@ -10,6 +10,21 @@ absolute_jump_opcodes = set((
     opcode.opmap['JUMP_IF_FALSE_OR_POP'], 
 ))
 
+relative_jump_opcodes = set((
+    opcode.opmap['SETUP_WITH'], 
+    opcode.opmap['JUMP_FORWARD'], 
+    opcode.opmap['FOR_ITER'], 
+    opcode.opmap['SETUP_LOOP'], 
+    opcode.opmap['SETUP_EXCEPT'], 
+    opcode.opmap['SETUP_FINALLY'], 
+))
+
+block_setup_opcodes = set((
+    opcode.opmap['SETUP_LOOP'], 
+    opcode.opmap['SETUP_EXCEPT'], 
+    opcode.opmap['SETUP_FINALLY'], 
+))
+
 def fix_absolute_jumps(opcodes, offsets):
     """
     When we mess with the bytecode and change offsets, any jumps that use
@@ -28,10 +43,32 @@ def fix_absolute_jumps(opcodes, offsets):
                 jump_to = ord(opcodes[i+1])
                 offset = sum(off[1] for off in offsets if off[0] <= jump_to)
                 new_opcodes += opcodes[i] + chr(ord(opcodes[i+1])+offset) + opcodes[i+2]
+            elif op in relative_jump_opcodes:
+                jump_to = ord(opcodes[i+1])
+                offset = sum(off[1] for off in offsets if i < off[0] <= jump_to)
+                new_opcodes += opcodes[i] + chr(ord(opcodes[i+1])+offset) + opcodes[i+2]
             else:
                 new_opcodes += opcodes[i:i+3]
             i += 3
     return new_opcodes
+
+def count_blocks(opcodes):
+    """
+    Counts the net number of blocks at the end of running the opcodes
+    """
+    i = 0
+    blocks = 0
+    while i < len(opcodes):
+        op = ord(opcodes[i])
+        if op < opcode.HAVE_ARGUMENT: #90, as mentioned earlier
+            if op == opcode.opmap['POP_BLOCK']:
+                blocks -= 1
+            i += 1
+        else:
+            if op in block_setup_opcodes:
+                blocks += 1
+            i += 3
+    return blocks
             
 def internal_loop(fxn):
     fco = fxn.__code__
@@ -41,6 +78,7 @@ def internal_loop(fxn):
     new_opcodes = ""
     last_idx = 0
     jump_offsets = []
+    blocks = 0
     for fxn_load, fxn_call, cur_num_args in find_tail_call(fxn):
         if cur_num_args > arg_count:
             print "Cannot tail call optimize tail call with variadic parameters: opcode ", fxn_call
@@ -54,7 +92,7 @@ def internal_loop(fxn):
             for var_idx in xrange(cur_num_args)
         ])
 
-        # load up actual code
+        # load up actual code before the recursion point
         new_opcodes += opcodes[last_idx:fxn_load]
 
         # skip over the loading of the recursed function onto the stack
@@ -68,7 +106,10 @@ def internal_loop(fxn):
         # function to start it all over again!
         # NOTE: offset is 5 here because we add 9 extra opcodes, but then
         # remove 4 when updating `last_idx`
-        jump_offsets.append((fxn_call, 5 + len(store_fast_args)))
+        # NOTE: we must also keep track of the size of the block stack so that
+        # we can pop back into the original frame of the function
+        blocks += count_blocks(opcodes[last_idx:fxn_call])
+        jump_offsets.append((fxn_call, 5 + len(store_fast_args) + blocks))
         new_opcodes += chr(opcode.opmap['BUILD_TUPLE']) + \
                        chr(cur_num_args) + \
                        NULL + \
@@ -76,6 +117,7 @@ def internal_loop(fxn):
                        chr(cur_num_args) + \
                        NULL + \
                        store_fast_args
+        new_opcodes += chr(opcode.opmap['POP_BLOCK']) * blocks
         new_opcodes += chr(opcode.opmap['JUMP_ABSOLUTE']) + \
                        chr(0) + \
                        NULL
@@ -91,3 +133,4 @@ def fib(i, current = 0, next = 1):
         return fib(i - 1, next, current + next)
     elif i == 0:
         return current
+
